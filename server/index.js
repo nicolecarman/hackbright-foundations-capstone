564e7ccd33527c139db6a532e2783b333666e480
NicoleCarman <nicolecarman90@outlook.com>
2022-09-13 09:48:26 -0500

    Updated for deployment

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ app.use(cors());
 
 // deployment
 app.use(express.static(path.join(__dirname, "../client")));
-/*
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 });
-*/
+
 app.get('/styles', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/styles.css'))
 });
